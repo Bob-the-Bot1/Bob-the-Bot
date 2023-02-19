0f8aa93c0028706e9b9c6f484e3ae62d68771613
csd4ni3l <96988024+csd4ni3l@users.noreply.github.com>
2023-02-19 17:31:54 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -12,15 +12,13 @@ import requests
 import bs4
 import json
 import math
-
+import asyncio
 import os
 
 
-
-
-guildId = 123456789
-intents = discord.Intents.default()
-intents.message_content = True
+guildId = 1037450276212318338
+intents = discord.Intents.all()
+intents.members = True
 client = discord.Bot(intents=intents)
 
 
@@ -29,34 +27,179 @@ client = discord.Bot(intents=intents)
 # Create an empty dictionary to store user data
 
 # Create an empty dictionary to store user data
-
-user_data = {}
+with open('levels.json', 'r') as file:
+    user_data = json.loads(file.read())
+with open('server_settings.json', 'r') as file:
+    server_settings = json.loads(file.read())
 
 # Define the amount of XP needed to level up
-level_up_xp = 150
+level_up_xp = 50
+
+#error handling
 
 
-# Function to add XP and level up the user
+@client.event
+async def on_command_error(context, error):
+  if isinstance(error, commands.MissingRequiredArgument):
+    await context.send("Oh no! Looks like you have missed out an argument for this command.")
+  elif isinstance(error, commands.MissingPermissions):
+    await context.send("Oh no! Looks like you Dont have the permissions for this command.")
+  elif isinstance(error, commands.MissingRole):
+    # bot errors
+    await context.send("Oh no! Looks like you Dont have the roles for this command.")
+  elif isinstance(error, commands.BotMissingPermissions):
+    await context.send("Oh no! Looks like I Dont have the permissions for this command.")
+  elif isinstance(error, commands.BotMissingRole):
+    await context.send("Oh no! Looks like I Dont have the roles for this command.")
+  elif isinstance(error, commands.CommandNotFound):
+    await context.send(f" :warning: {error}.")
+  elif isinstance(error, commands.MemberNotFound):
+    await context.send("Please specify the member correctly!!!!!!")
+  else:
+    raise error
+
+
+@client.event
+async def on_message(msg: discord.Message):
+    if not msg.author.bot:
+        if str(msg.author.id) not in user_data:
+            user_data[str(msg.author.id)] = {"xp": 0, "level": 1}
+        user_data[str(msg.author.id)]["xp"] += 1
+        if user_data[str(msg.author.id)]["xp"] >= level_up_xp:
+            user_data[str(msg.author.id)]["level"] += 1
+            if server_settings.get(str(msg.guild.id)) != None and server_settings[str(msg.guild.id)]['levelup_channel'] != None:
+                await discord.utils.get(msg.guild.channels, id=server_settings[ste(msg.guild.id)]['levelup_channel']).send(f'{msg.author.mention} just reached level {user_data[str(msg.author.id)]["level"]}!')
+            user_data[str(msg.author.id)]["xp"] = 0
+
+        with open('levels.json', 'w') as file:
+            file.write(json.dumps(user_data, indent=4))
+
+
+@client.slash_command()
+@commands.has_permissions(administrator=True)
+async def raid_mode(ctx):
+    if server_settings.get(str(ctx.guild.id)) != None:
+        if server_settings[str(ctx.guild.id)].get('raid_mode') != None:
+            server_settings[str(ctx.guild.id)]['raid_mode'] = not server_settings[str(ctx.guild.id)]['raid_mode']
+        else:
+            server_settings[str(ctx.guild.id)]['raid_mode'] = True
+    else:
+        server_settings[str(ctx.guild.id)] = {}
+        server_settings[str(ctx.guild.id)]['raid_mode'] = False
+        server_settings[str(ctx.guild.id)]['levelup_channel'] = None
+        server_settings[str(ctx.guild.id)]['welcome_channel'] = None
+    with open('server_settings.json', 'w') as file:
+        file.write(json.dumps(server_settings, indent=4))
+
+
+@client.event
+async def on_member_join(member):
+    if server_settings.get(str(member.guild.id)) and server_settings[str(member.guild.id)].get('raid_mode', False):
+        await member.ban('raid mode was activated when you joined')
+        return
+
+    if server_settings.get(str(member.guild.id)) != None and server_settings[str(member.guild.id)]['welcome_channel'] != None:
+        await discord.utils.get(member.guild.channels, id=server_settings[str(member.guild.id)]['welcome_channel']).send(f'{member.mention} just joined the server!')
+
+
+@client.slash_command()
+async def set_levelup_channel(ctx, channel: discord.TextChannel):
+    if server_settings.get(str(ctx.guild.id)) != None:
+        server_settings[str(ctx.guild.id)]['levelup_channel'] = channel.id
+    else:
+        server_settings[str(ctx.guild.id)] = {}
+        server_settings[str(ctx.guild.id)]['levelup_channel'] = channel.id
+        server_settings[str(ctx.guild.id)]['welcome_channel'] = None
+    with open('server_settings.json', 'w') as file:
+        file.write(json.dumps(server_settings, indent=4))
+
+
+@client.slash_command()
+async def set_welcome_channel(ctx, channel: discord.TextChannel):
+    if server_settings.get(str(ctx.guild.id)) != None:
+        server_settings[str(ctx.guild.id)]['welcome_channel'] = channel.id
+    else:
+        server_settings[str(ctx.guild.id)] = {}
+        server_settings[str(ctx.guild.id)]['welcome_channel'] = channel.id
+        server_settings[str(ctx.guild.id)]['levelup_channel'] = None
+    with open('server_settings.json', 'w') as file:
+        file.write(json.dumps(server_settings, indent=4))
+
+
+@client.slash_command()
+@commands.has_permissions(manage_channels=True)
+async def lock(ctx, _channel: discord.TextChannel = None):
+    if _channel == None:
+        channel = ctx.channel
+    else:
+        channel = _channel
+    await channel.set_permissions(ctx.guild.default_role, send_messages=False)
+    await ctx.respond(f'Channel locked by {ctx.author.mention}')
+
+
+@client.slash_command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, limit: int):
+    await ctx.channel.purge(limit=limit)
+    await ctx.respond(f'{limit} message(s) were deleted by {ctx.author.mention}')
+
+
+@client.slash_command()
+@commands.has_permissions(manage_messages=True)
+async def say(ctx, *, msg):
+	await ctx.send(msg)
+
+
+@client.slash_command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason='he/she was bad'):
+    if ctx.author.top_role.position > member.top_role.position:
+        await member.ban()
+        await ctx.respond(str(member)+' was banned by '+str(ctx.author)+', because '+reason)
+    else:
+        await ctx.respond('You need to be above a member to ban!')
+
+
+@client.slash_command()
+@commands.has_permissions(ban_members=True)
+async def unban(ctx, member: discord.Member):
+    await member.unban()
+    await ctx.respond(str(member)+'was unbanned by '+str(ctx.author))
+
+
+@client.slash_command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason='he/she was bad'):
+    if ctx.author.top_role.position > member.top_role.position:
+        await member.kick(reason=reason)
+        await ctx.respond(str(member)+' was kicked by '+str(ctx.author)+',because '+reason)
+    else:
+        await ctx.respond('You need to be above a member to kick!')
+
+
 def add_xp(user, xp):
     if user not in user_data:
         user_data[user] = {"xp": 0, "level": 1}
     user_data[user]["xp"] += xp
     if user_data[user]["xp"] >= level_up_xp:
         user_data[user]["level"] += 1
+
         user_data[user]["xp"] = 0
+    with open('levels.json', 'w') as file:
+        file.write(json.dumps(user_data, indent=4))
 
 
 @client.slash_command(
-    name="level",
+    name="rank",
     description="View your current level",
 
 )
-async def level(interaction, member: discord.Member):
+async def rank(interaction, member: discord.Member):
     if member is None:
         member = interaction.message.author
-    if member in user_data:
+    if str(member.id) in user_data:
         await interaction.response.send_message(
-            f"{member.mention} is currently level {user_data[member]['level']} with {user_data[member]['xp']} XP."
+            f"{member.mention} is currently level {user_data[str(member.id)]['level']} with {user_data[str(member.id)]['xp']} XP."
         )
     else:
         await interaction.respond(
@@ -67,16 +210,21 @@ async def level(interaction, member: discord.Member):
 @client.slash_command(
     name="givexp", description="Give XP to a user"
 )
-async def givexp(interaction, member: discord.Member, xp: int):
-    if member.id not in user_data:
-        user_data[member.id] = {"xp": 0, "level": 1}
-    user_data[member.id]["xp"] += xp
-    user_data[member.id]["level"] = math.floor(
-        0.1 * math.sqrt(user_data[member.id]["xp"])
-    )
+async def givexp(interaction: discord.Interaction, member: discord.Member, xp: int):
+    if str(member.id) not in user_data:
+        user_data[str(member.id)] = {"xp": 0, "level": 1}
+    user_data[str(member.id)]["xp"] += xp
+    while user_data[str(member.id)]["xp"] >= level_up_xp:
+        user_data[str(member.id)]["xp"] -= level_up_xp
+        user_data[str(member.id)]["level"] += 1
+        if server_settings.get(str(interaction.guild.id)) != None and server_settings[str(interaction.guild.id)].get("levelup_channel") != None:
+            level = user_data[str(member.id)]["level"]
+            await discord.utils.get(interaction.guild.channels,id=server_settings[str(interaction.guild.id)]['levelup_channel']).send(f'{member.mention} just reached level {level}!')
     await interaction.respond(
-        f"{member.mention} has been given {xp} XP. They now have {user_data[member.id]['xp']} XP and are level {user_data[member.id]['level']}."
+        f"{member.mention} has been given {xp} XP. They now have {user_data[str(member.id)]['xp']} XP and are level {level}."
     )
+    with open('levels.json', 'w') as file:
+        file.write(json.dumps(user_data, indent=4))
     print(Fore.GREEN + f"xp was given to {member.mention}")
 
 
@@ -85,8 +233,11 @@ client.slash_command(
     description="View the leaderboard"
 
 )
+
+
 async def leaderboard(interaction):
-    leaderboard = sorted(user_data.items(), key=lambda x: x[1]["level"], reverse=True)
+    leaderboard = sorted(
+        user_data.items(), key=lambda x: x[1]["level"], reverse=True)
     embed = discord.Embed(title="Leaderboard", color=0x00FF33)
     for i, user in enumerate(leaderboard):
         member = interaction.message.guild.get_member(int(user[0]))
@@ -114,15 +265,19 @@ async def ping(interaction):
 )
 @commands.has_permissions(manage_roles=True)
 async def giverole(interaction, member: discord.Member, role: discord.Role):
-    await member.add_roles(role)
-    await interaction.respond(
-        f"{member.mention} has been given the {role.name} role."
-    )
+
+    if member.top_role.position > role.position:
+        await member.add_roles(role)
+        await interaction.respond(
+            f"{member.mention} has been given the {role.name} role.")
+    else:
+        await interaction.respond(f"{member.mention} You cant give a role to yourself thats higher than your top role!")
+
 
 @client.slash_command(
     name="remrole",
     description="Removes a role from a user"
-    
+
 )
 @commands.has_permissions(manage_roles=True)
 async def remrole(interaction, member: discord.Member, role: discord.Role):
@@ -130,6 +285,7 @@ async def remrole(interaction, member: discord.Member, role: discord.Role):
     await interaction.respond(
         f"{member.mention} has been removed from the {role.name} role."
     )
+
 
 @client.slash_command(
     name="bored",
@@ -168,7 +324,7 @@ async def quote(interaction):
 @client.slash_command(name="hello", description="Say hi!")
 async def hello(interaction):
     greetings = ["Hello", "Hi", "Greetings", "Hola", "Bonjour", "Konnichiwa"]
-    username = interaction.message.author.name
+    username = interaction.author.name
     await interaction.respond(f"{random.choice(greetings)} {username}!")
 
 
@@ -185,7 +341,7 @@ async def info(interaction):
     embed.set_author(name="Bob the Bot")
     embed.add_field(
         name="Developers",
-        value="ExplodeCode \n OpenSourceSimon \n Tim \n Cattopy The Web",
+        value="ExplodeCode \n OpenSourceSimon \n Tim \n Cattopy The Web \n csdaniel\(revamper,hoster\)",
         inline=False,
     )
     await interaction.respond(embed=embed)
@@ -205,15 +361,6 @@ async def cal(interaction):
 @client.slash_command(
     name="search", description="Search the internet."
 )
-
-
-
-
-
-
-
-
-
 async def search(interaction, query: str, engine: str):
     print(engine)  # Choice(name='Google', value='google')
     engine = engine.value
@@ -322,7 +469,9 @@ async def on_ready():
     print(Fore.CYAN + f"Successfully logged in as {client.user}")
     print("Packages and commands are loaded")
     print(Fore.GREEN + "-------------------")
-    print(Fore.GREEN + f"Thanks for using Bob the Bot! {client.user} is now online")
+    print(Fore.GREEN +
+          f"Thanks for using Bob the Bot! {client.user} is now online")
 
 
-client.run('TOKEN') 
+client.run(
+    'TOKEN')
